80ed26e1201208ca555bd0b6e520e4c698ac698f
Carlos <carlos.e.corona@hotmail.com>
2026-08-19 18:58:58 -0600

    Practica-2

---
+++ src/Animales/Perro.java
@@ -0,0 +1,26 @@
+package Animales;
+
+public class Perro extends Animal {
+    private String raza;
+    private boolean estaVacunado;
+
+    public Perro(String nombre, int edad, double peso, String raza, boolean estaVacunado) {
+        super(nombre, edad, peso);
+        this.raza = raza;
+        this.estaVacunado = estaVacunado;
+    }
+
+    public void ladrar() {
+        System.out.println(getNombre() + " dice: Guau, guau!.");
+    }
+
+    public void buscarPelota() {
+        System.out.println(getNombre() + " esta buscando la pelota.....");
+    }
+    
+    @Override
+    public String toString() {
+        return "Nombre=" + getNombre() + "| Edad=" + getEdad() + " años" + "| Peso=" + getPeso() + " kg" + "| Raza=" + raza + "| Vacunado=" + estaVacunado ;
+    }
+    
+}
